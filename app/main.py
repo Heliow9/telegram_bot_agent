4a7a176229d3627556feb97683318e6b1d8dd71b
Helio Livramento <helio.paraiso@gmail.com>
2026-04-15 00:42:10 -0300

    corrigindo cors api

--- app/main.py
+++ app/main.py
@@ -82,6 +82,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://telegram-bot-agent-6gik.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
