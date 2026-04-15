ca6c69c4f8a8bb52f646aff09d0393a4220296c3
Helio Livramento <helio.paraiso@gmail.com>
2026-04-15 01:14:33 -0300

    corrigindo api3

--- app/main.py
+++ app/main.py
@@ -82,7 +82,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://telegram-bot-agent-6gik.onrender.com"
+        "https://bot-bet-front.onrender.com/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
